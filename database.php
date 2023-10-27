874e21056f70bb5157102b37518c83d282503478
rahuldk1105 <149181581+rahuldk1105@users.noreply.github.com>
2023-10-28 00:34:16 +0530

    database.php

--- database.php
+++ database.php
@@ -1,27 +1,37 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Define the path to the text file where you want to store the data
-    $file = 'user_data.txt';
+// Database connection details
+$servername = "your_servername";
+$username = "your_username";
+$password = "your_password";
+$database = "your_database";
 
-    // Get form data
-    $firstName = $_POST['first-name'];
-    $lastName = $_POST['last-name'];
-    $email = $_POST['email'];
-    $password = $_POST['new-password'];
-    $accountType = isset($_POST['account-type']) ? $_POST['account-type'] : '';
-    $profilePicture = isset($_FILES['file']) ? $_FILES['file']['name'] : '';
-    $age = $_POST['age'];
-    $referrer = $_POST['referrer'];
-    $bio = $_POST['bio'];
-
-    // Create a formatted string with the data
-    $data = "First Name: $firstName\nLast Name: $lastName\nEmail: $email\nPassword: $password\nAccount Type: $accountType\nProfile Picture: $profilePicture\nAge: $age\nReferrer: $referrer\nBio: $bio\n\n";
-
-    // Append the data to the text file
-    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-
-    echo "Data has been saved successfully.";
-} else {
-    echo "Invalid request.";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
+
+// Retrieve form data
+$first_name = $_POST['first-name'];
+$last_name = $_POST['last-name'];
+$email = $_POST['email'];
+$age = $_POST['age'];
+$student = $_POST['Are You a student'];
+$rating = $_POST['rating'];
+$suggestions = $_POST['suggestions'];
+
+// SQL query to insert the data into a table
+$sql = "INSERT INTO survey_data (first_name, last_name, email, age, student, rating, suggestions) 
+        VALUES ('$first_name', '$last_name', '$email', '$age', '$student', '$rating', '$suggestions')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Form data submitted successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
 ?>
